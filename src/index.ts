46d5fb9523bf86564a1dd420bffd9378cc40f34e
Robert Michalski <robert@surveylegend.com>
2018-01-18 10:16:29 +0100

    Removed dependency on lodash.some and lodash.castarray, use native js instead.

--- src/index.ts
+++ src/index.ts
@@ -2,17 +2,15 @@ import { PluginFunction } from 'hapi';
 import { isEqual as ipIsEqual } from 'ip';
 import { getClientIp } from 'request-ip';
 import { unauthorized } from 'boom';
-import castArray = require('lodash.castarray');
-import some = require('lodash.some');
 
 export const register: PluginFunction<{}> = function(server, options, next) {
   server.auth.scheme('ip-whitelist', function(server, whitelisted: string | string[]) {
-    const list = castArray(whitelisted);
+    const list = whitelisted instanceof Array ? whitelisted : [whitelisted];
 
     return {
       authenticate(request, reply) {
         const remoteAddress = getClientIp(request);
-        if (some(list, (address) => ipIsEqual(remoteAddress, address))) {
+        if (list.some(address => ipIsEqual(remoteAddress, address))) {
           reply.continue({ credentials: remoteAddress });
         } else {
           reply(unauthorized(`${remoteAddress} is not a valid IP`));
