408e422fa8e1672f3de6cb9cb8154c0149d20771
Robert Michalski <robert@surveylegend.com>
2018-01-19 10:07:27 +0100

    Removed lodah.some from dependencies.

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,6 @@
 import { PluginFunction } from 'hapi';
 import { isEqual as ipIsEqual } from 'ip';
 import { unauthorized } from 'boom';
-import some = require('lodash.some');
 
 export const register: PluginFunction<{}> = function(server, options, next) {
   server.auth.scheme('ip-whitelist', function(server, whitelisted: string | string[]) {
@@ -11,7 +10,7 @@ export const register: PluginFunction<{}> = function(server, options, next) {
       authenticate(request, reply) {
         // in case you are behind a proxy, use Hapi plugin `therealyou`
         const { remoteAddress } = request.info;
-        if (some(list, (address) => ipIsEqual(remoteAddress, address))) {
+        if (list.some(address => ipIsEqual(remoteAddress, address))) {
           reply.continue({ credentials: remoteAddress });
         } else {
           reply(unauthorized(`${remoteAddress} is not a valid IP`));
