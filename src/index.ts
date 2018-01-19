f4a9c046307229296dcb0c71dddd03ca26018162
Robert Michalski <robert@surveylegend.com>
2018-01-19 17:48:36 +0100

    Applied requested fixes from review.

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,13 @@
-import { PluginFunction, Request, Server, ReplyWithContinue } from 'hapi';
+import { PluginFunction, Request, Server, ReplySchemeAuth } from 'hapi';
 import { isEqual as ipIsEqual } from 'ip';
 import { unauthorized } from 'boom';
 
-export const register: PluginFunction<{}> = function(server:Server, options, next) {
-  server.auth.scheme('ip-whitelist', function(serverAuth:Server, whitelisted: string | string[]) {
+export const register: PluginFunction<{}> = function(server: Server, options, next) {
+  server.auth.scheme('ip-whitelist', function(serverAuth: Server, whitelisted: string | string[]) {
     const list = whitelisted instanceof Array ? whitelisted : [whitelisted];
-    //const list = [].concat(whitelisted); // TypeScript compiler complains but generates correct code
 
     return {
-      authenticate(request:Request, reply:ReplyWithContinue) {
+      authenticate(request: Request, reply: ReplySchemeAuth) {
         const { remoteAddress } = request.info;
         if (list.some(address => ipIsEqual(remoteAddress, address))) {
           reply.continue({ credentials: remoteAddress });
