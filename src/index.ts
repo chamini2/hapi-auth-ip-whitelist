67429fb9e204221d1d342a11ba63bd0d760b594d
Robert Michalski <robert@surveylegend.com>
2018-01-19 10:00:43 +0100

    Removed lodah.castarray from dependencies.

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,11 @@
 import { PluginFunction } from 'hapi';
 import { isEqual as ipIsEqual } from 'ip';
 import { unauthorized } from 'boom';
-import castArray = require('lodash.castarray');
 import some = require('lodash.some');
 
 export const register: PluginFunction<{}> = function(server, options, next) {
   server.auth.scheme('ip-whitelist', function(server, whitelisted: string | string[]) {
-    const list = castArray(whitelisted);
+    const list = whitelisted instanceof Array ? whitelisted : [whitelisted]
 
     return {
       authenticate(request, reply) {
